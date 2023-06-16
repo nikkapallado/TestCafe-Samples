732b39748dd69c2728e6a83a432079ef8e0b669d
nikkapallado <ntpallado@gmail.com>
2023-06-16 10:10:33 +0800

    feat: added navigate to example page test and applied .only

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,17 +1,17 @@
 const conf = require("../config/conf.json");
 const example_page_data = require("../test-data/examplePageData.json");
 // const {home_page_object} = require("../page-objects/homePageObject.js");
-import {Selector} from 'testcafe';
+import { Selector } from 'testcafe';
 
 const developer_name = Selector('#developer-name');
 const os_option = Selector('#windows');
 const submit_button = Selector('#submit-button');
 
 fixture('First fixture')
-    .meta('version','1.0.0')
+    .meta('version', '1.0.0')
     .page(conf.base_url);
 
-test.meta('env','production')
+test.meta('env', 'production')
     .page(`${conf.base_url}/example/`)
     ('First test', async test_controller => {
         await test_controller
@@ -22,7 +22,7 @@ test.meta('env','production')
         // home_page.enterDevName(home_page_data.first_test.dev_name);
         // home_page.chooseOsOption();
         // home_page.clickSubmitButton();
-} );
+    });
 
 test.page(`${conf.base_url}/example/`)
     ('Second test', async test_controller => {
@@ -30,4 +30,11 @@ test.page(`${conf.base_url}/example/`)
             .typeText(developer_name, 'Bett Lafea')
             .click(os_option)
             .click(submit_button);
-} );
+    });
+
+test.only
+    ('Navigate to example page', async test_controller => {
+        await test_controller
+            .navigateTo(`${conf.base_url}/example/`)
+            .expect(Selector('h1').textContent).eql('Example')
+    });
